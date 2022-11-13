7700de09b63d815b700d397bb4554749ef008d15
chan <white1614@naver.com>
2022-11-13 12:04:51 +0900

    기본설정
    웹페이지 만들기 전 기본 설정 완료

--- index.js
+++ index.js
@@ -1,1 +1,14 @@
-"use strict"
+const express = require('express');
+const app =express();
+const path = require('path');
+
+app.set('view engine', 'ejs');
+app.set('views', path.join(__dirname, 'views'));
+
+app.get('/', (req,res) => {
+    res.render('home')
+})
+
+app.listen(3000, (req,res) => {
+    console.log('서버 가동')
+})
