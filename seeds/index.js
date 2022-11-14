1642fae613d0d730d5e20e58f39275ebcdb027fd
chan <white1614@naver.com>
2022-11-14 15:03:17 +0900

    이미지 추가
    이미지 추가

--- seeds/index.js
+++ seeds/index.js
@@ -1,12 +1,12 @@
 const mongoose = require('mongoose');
 const Campground = require('../models/campground');
 const cities = require('./cities');
-const { places, descriptors } =require('./seedHelpers')
+const { places, descriptors } = require('./seedHelpers')
 
 mongoose.connect('mongodb://0.0.0.0:27017/chanCamp')
     .then(() => {
         console.log('MONGODB CONNECT!!')
-    })  
+    })
     .catch(err => {
         console.log('에러가 발생했습니다. ')
         console.log(err)
@@ -16,11 +16,14 @@ const sample = array => array[Math.floor(Math.random() * array.length)]
 
 const seedDB = async () => {
     await Campground.deleteMany({});
-    for(let i = 0; i < 15; i++) {
+    for (let i = 0; i < 15; i++) {
         const random1000 = Math.floor(Math.random() * 1000);
         const camp = new Campground({
             location: `${cities[random1000].city}, ${cities[random1000].state}`,
-            title: `${sample(places)}, ${sample(descriptors)}`
+            title: `${sample(places)}, ${sample(descriptors)}`,
+            image: 'https://source.unsplash.com/collection/483251',
+            description: 'hello this is chan camp!!',
+            price: 13
         })
         await camp.save()
     }
