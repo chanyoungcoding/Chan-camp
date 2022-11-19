3b0838ba6fba68700d224ac4f46920f6607d6d01
chan <white1614@naver.com>
2022-11-19 22:30:54 +0900

    flash,session 추가 및 메시지 출력
    flash와 session 을 추가하고
    성공했을때와 오류가 났을때
    메시지를 띄우는 기능을 추가

--- app.js
+++ app.js
@@ -5,6 +5,8 @@ const mongoose = require('mongoose');
 const methodOverride = require('method-override')
 const ejsMate = require('ejs-mate');
 const ExpressError = require('./utils/ExpressError')
+const session = require('express-session')
+const flash = require('connect-flash')
 
 const campgrounds = require('./routes/campgrounds')
 const reviews = require('./routes/reviews')
@@ -25,6 +27,26 @@ app.set('views', path.join(__dirname, 'views'));
 app.use(express.urlencoded({extended: true}))
 app.use(methodOverride('_method'))
 app.use(express.static(path.join(__dirname, 'public')))
+
+const sessionConfig = {
+    secret: 'chanSecret',
+    resave: false,
+    saveUninitialized: true,
+    cookie: {
+        httpOnly: true,
+        expires: Date.now() + 1000 * 60 * 60 * 24 * 7,
+        maxAge: 1000 * 60 * 60 * 24 * 7
+    }
+}
+
+app.use(session(sessionConfig))
+app.use(flash())
+
+app.use((req, res, next) => {
+    res.locals.success = req.flash('success');
+    res.locals.error = req.flash('error')
+    next();
+})
 
 app.use('/campgrounds', campgrounds)
 app.use('/campgrounds/:id/reviews', reviews)
--- routes/campgrounds.js
+++ routes/campgrounds.js
@@ -28,30 +28,39 @@ router.get('/new', (req,res) => {
 router.post('/', validateCampground ,catchAsync(async(req,res) => {
     const campground = new Campground(req.body.campground)
     await campground.save();
+    req.flash('success', 'Successfully made a new Campground')
     res.redirect(`/campgrounds/${campground._id}`)
 }))
 
 router.get('/:id', catchAsync(async(req,res) => {
     const { id } = req.params
     const campground = await Campground.findById(id).populate('reviews')
+    if (!campground) {
+        req.flash('error', 'Cannot find that campground!!')
+    }
     res.render('campgrounds/show', { campground })
 }))
 
 router.get('/:id/edit', catchAsync(async(req,res) => {
     const { id } = req.params
     const campground = await Campground.findById(id)
+    if (!campground) {
+        req.flash('error', 'Cannot find that campground!!')
+    }
     res.render('campgrounds/edit', { campground })
 }))
 
 router.put('/:id', validateCampground ,catchAsync(async(req,res) => {
     const { id } = req.params
-    const campground = await Campground.findByIdAndUpdate(id, {...req.body.campground})
+    const campground = await Campground.findByIdAndUpdate(id, { ...req.body.campground })
+    req.flash('success', 'Successfully updated Campground!!')
     res.redirect(`/campgrounds/${campground._id}`)
 }))
 
 router.delete('/campgrounds/:id', validateCampground ,catchAsync(async(req,res) => {
     const { id } = req.params
     await Campground.findByIdAndDelete(id)
+    req.flash('success', 'Successfully deleted Campground!!')
     res.redirect('/campgrounds')
 }))
 
